4cb0241b540e3158364e080f396170f69983cb32
Pawan Jain <jainpawan211199@gmail.com>
2019-02-18 22:43:37 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristornate Con Fusion</NavbarBrand>
 
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   } 

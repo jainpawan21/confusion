b69f1938d937a304eb5374cf9696385f480285b4
Pawan Jain <jainpawan211199@gmail.com>
2019-02-19 00:17:08 +0530

    Components part 2 something is not working

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <div>
+      <div >
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristornate Con Fusion</NavbarBrand>
 
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   } 

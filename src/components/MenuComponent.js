ddf966b46bfb0ddb813b8a10e28dda567b0b1d6b
Pawan Jain <jainpawan211199@gmail.com>
2019-02-20 18:26:53 +0530

    Components Life Cycle

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,6 +9,10 @@ class Menu extends Component{
         this.state = {
             selectedDish : null
         }
+        console.log('Menu Component Constructor is invoked');
+    }
+    componentDidMount(){
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish)
@@ -53,6 +57,8 @@ class Menu extends Component{
                 </div>
             );
         });
+         
+        console.log('Menu Component render is invoked');
         
         return( 
             <div classNme="container">

03dacf70bef95eec601b302e8210678fc17408b1
PAWAN JAIN <jainpawan211199@gmail.com>
2019-06-15 19:26:48 +0530

    redux-thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,8 +6,10 @@ import {
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent'
 
-//REDUX VALIDATION
+
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -162,8 +164,26 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
+    if(props.isLoading){
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess){
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
 
-    if (props.dish != null)
+    }
+    else if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">

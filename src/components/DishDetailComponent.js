3567a15b6316f4d5bd53ac73da0c4e463af23bd8
PAWAN JAIN <jainpawan211199@gmail.com>
2019-06-15 16:08:28 +0530

    combine reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,57 +1,192 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle
-  , BreadcrumbItem ,Breadcrumb } from 'reactstrap';
-import { Link } from 'react-router-dom'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, Button,
+    Modal, ModalHeader, ModalBody, Row, Col, Label
+} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
 
-export default function SelectedDishes(props) {   
-    if (props.dis != null) {
+//REDUX VALIDATION
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
-    const renderComments = (dishComments) => dishComments.map(comment => {
-        return (
-        <div key={comment.id}> 
-        <p> {comment.comment} </p>
-        <p> --{comment.author}, {new Intl.DateTimeFormat('en-US',{year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-        </div>
-        ) 
-    });
-
-    return(
-        <div className="container">
-        <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dis.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dis.name}</h3>
-                    <hr />
-                </div>
-        </div>
-                  <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                      <Card>
-                        <CardImg top src={props.dis.image} alt={props.dis.name} />
-                        <CardBody>
-                          <CardTitle>{props.dis.name}</CardTitle>
-                          <CardText>{props.dis.description}</CardText>
-                        </CardBody>
-                      </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                      <h4>Comments</h4>
-                       {renderComments(props.comments)}
-                    </div>
-                  </div>
-        </div>
-    
-    );
-       }
-else
-    return(
-      
-        <div>
-            
-            <h1>PAWAN JAIN</h1>
+function RenderDish({ dish }) {
+    return (
+        <div class="container">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
         </div>
     );
 }
+
+
+function RenderComments({ comments }) {
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const comments_2 = comments.map((comm) => {
+        var dat = new Date(Date.parse(comm.date))
+        var month = monthNames[dat.getMonth()];
+        var day = dat.getDay();
+        var year = dat.getFullYear();
+        return (
+            <div key={comm.id}>
+                {comm.comment}
+                <br></br>
+                <br></br>
+                --{comm.author}, {month + " " + day + ", " + year}
+                <br></br>
+                <br></br>
+            </div>
+        );
+    });
+    return (
+        <div>
+            <h4>Comments</h4>
+            <ul className='list-unstyled'>
+                {comments_2}
+            </ul>
+        </div>
+    );
+}
+
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+   //WITH REDUX
+   handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+    // event.preventDefault();
+}
+
+    render() {
+        return (
+            <React.Fragment>
+                <Button outline onClick={this.toggleModal} color="secondary"> <i className="fa fa-pencil"></i> Submit Comments</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={12}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" name="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="name" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".name" id="name" name="name"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+
+                            <Row className="form-group">
+                                <Label htmlFor="feedback" md={12}>Your Feedback</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".feedback" id="feedback" name="feedback"
+                                        rows="12"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{ size: 10, offset: 0 }}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
+
+const DishDetail = (props) => {
+
+    if (props.dish != null)
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                        <CommentForm />
+                    </div>
+                </div>
+            </div>
+        );
+
+    else
+        return (
+            <div></div>
+        );
+}
+
+export default (DishDetail);
+
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent'
-import SelectedDishes from "./DishDetailComponent";
+import DishDetail from "./DishDetailComponent";
 import Header from './HeaderComponent'
 import Footer from './FooterComponent' 
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
@@ -40,7 +40,7 @@ class Main extends Component {
 
     const DishWithID = ({match}) => {
       return(
-        <SelectedDishes dis={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
           />
 
